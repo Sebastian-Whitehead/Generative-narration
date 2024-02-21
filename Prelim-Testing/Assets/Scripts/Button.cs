2edc9d042fa029e4cb7e3876c1c8ad05703631a1
Sebastian W <sebastianmikael2000@gmail.com>
2024-02-21 11:48:23 +0100

    reusing assets
    Copying Assets from Narratives project

--- Prelim-Testing/Assets/Scripts/Button.cs
+++ Prelim-Testing/Assets/Scripts/Button.cs
@@ -10,13 +10,7 @@ public class Button : MonoBehaviour
     public float maxDistance = 5f;
 
     [Tooltip("List of objects bound to this button.")]
-    public List<MonoBehaviour> boundObjects = new List<MonoBehaviour>();
-
-    [Tooltip("Delay before triggering the final triggers.")]
-    public float delayBeforeFinalTriggers = 0f;
-    
-    [Tooltip("List of final triggers that will run after bound triggers.")]
-    public List<MonoBehaviour> finalTriggers = new List<MonoBehaviour>();
+    public MonoBehaviour boundObject;
 
     [Tooltip("Sound played when the button is pressed.")]
     public AudioClip buttonSound;
@@ -24,20 +18,15 @@ public class Button : MonoBehaviour
     [Tooltip("Key code to trigger the button.")]
     public KeyCode inputKey;
 
-    private List<ITrigger> _mBoundTriggers = new List<ITrigger>();
-    private List<ITrigger> _mFinalTriggers = new List<ITrigger>();
-    private AudioSource _audioSource;
+    [Tooltip("Delay between trigger and action.")]
+    public float triggerDelay;
 
-    private bool _boundTriggersCompleted = false;
+    private ITrigger _mBoundTrigger;
+    private AudioSource _audioSource;
 
     void Start()
     {
-        _mBoundTriggers = boundObjects.OfType<ITrigger>().ToList();
-        boundObjects.Clear();
-
-        _mFinalTriggers = finalTriggers.OfType<ITrigger>().ToList();
-        finalTriggers.Clear();
-
+        _mBoundTrigger = boundObject.GetComponent<ITrigger>();
         _audioSource = GetComponent<AudioSource>();
     }
 
@@ -57,54 +46,9 @@ public class Button : MonoBehaviour
 
     IEnumerator TriggerBoundObjects()
     {
-        float totalWaitForCompletionDuration = 0f;
-        bool narrationTriggerEncountered = false;
-
-        foreach (var trigger in _mBoundTriggers)
-        {
-            trigger.Trigger();
-
-            if (trigger.Type == Fx.Narration)
-            {
-                narrationTriggerEncountered = true;
-                // Add the duration of the Narration trigger
-                totalWaitForCompletionDuration += trigger.Duration;
-            }
-
-            if (!narrationTriggerEncountered && trigger.WaitForCompletion)
-            {
-                // If WaitForCompletion is true and narration has not been encountered,
-                // include the duration in the totalWaitForCompletionDuration
-                totalWaitForCompletionDuration += trigger.Duration;
-            }
-
-            if (trigger.WaitForCompletion)
-            {
-                yield return new WaitForSeconds(trigger.Duration);
-            }
-        }
-
-        // Mark bound triggers as completed
-        _boundTriggersCompleted = true;
-
-        // Check if both bound triggers and final triggers are completed
-        if (_boundTriggersCompleted)
-        {
-            // Calculate the time to wait before triggering final triggers
-            float delay = totalWaitForCompletionDuration + delayBeforeFinalTriggers;
-
-            // Delay before triggering final triggers
-            yield return new WaitForSeconds(delay);
-
-            foreach (var finalTrigger in _mFinalTriggers)
-            {
-                finalTrigger.Trigger();
-
-                if (finalTrigger.WaitForCompletion)
-                {
-                    yield return new WaitForSeconds(finalTrigger.Duration);
-                }
-            }
-        }
+        //Delay trigger
+        yield return new WaitForSeconds(triggerDelay);
+        
+        _mBoundTrigger.Trigger();
     }
 }
