5aef00b4dc6a25d2471bc30f10607d84e10af206
Mads Wagner <mads.w.s@hotmail.com>
2024-03-20 13:24:43 +0100

    Added multiple voiceIDs
    TTSManager and InputField gameobjects don't get pushed for whatever reason

--- Prelim-Testing/Assets/Scripts/TextToSpeech.cs
+++ Prelim-Testing/Assets/Scripts/TextToSpeech.cs
@@ -4,11 +4,13 @@ using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.Networking;
 
-// This class is responsible for converting text to speech.
 public class TextToSpeech : MonoBehaviour
 {
-    // The ID of the voice to be used for speech synthesis.
-    public string voiceID = "21m00Tcm4TlvDq8ikWAM";
+    // The names of the voices to be used for speech synthesis.
+    public string[] voiceNames = {"Rachel", "Dorothy", "Michael", "George"};
+    // The IDs of the voices to be used for speech synthesis.
+    private string[] voiceIDs = {"21m00Tcm4TlvDq8ikWAM", "ThT5KcBeYPX3keUQqHPh", "flq6f7yk4E4fJM5XTYuZ", "JBFqnCBsd6RMkjVDRZzb"};
+    public int selectedVoice = 0; // Default to the first voice
     string url;
     // The API key for the Eleven Labs Text-to-Speech service.
     public string xiApiKey = "769852b4a6ecf3b3008813ef2d8355b7";
@@ -22,9 +24,7 @@ public class TextToSpeech : MonoBehaviour
 
     void Start()
     {
-        // Construct the URL for the Eleven Labs Text-to-Speech service.
-        url = "https://api.elevenlabs.io/v1/text-to-speech/" + voiceID + "/stream";
-
+        UpdateUrl();
         audioSource = GetComponent<AudioSource>();
     }
 
@@ -37,9 +37,17 @@ public class TextToSpeech : MonoBehaviour
         }
     }
 
+    void UpdateUrl()
+    {
+        // Construct the URL for the Eleven Labs Text-to-Speech service.
+        url = "https://api.elevenlabs.io/v1/text-to-speech/" + voiceIDs[selectedVoice] + "/stream";
+    }
+
     // This coroutine generates the speech from the input text.
     IEnumerator Generate(string inputText)
     {
+        UpdateUrl();
+
         // Construct the JSON data for the POST request.
         string jsonData = "{\"text\":\"" + inputText + "\",\"model_id\":\"" + modelId + "\",\"voice_settings\":{\"stability\":" + stability.ToString(System.Globalization.CultureInfo.InvariantCulture) + ",\"similarity_boost\":" + similarityBoost.ToString(System.Globalization.CultureInfo.InvariantCulture) + "}}";
 
