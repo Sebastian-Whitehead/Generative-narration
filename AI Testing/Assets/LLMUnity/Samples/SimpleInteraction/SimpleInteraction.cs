c15afc8770a2b60bb46d88624ec550e83ed3ff81
Sebastian W. <sebastianmikael2000@gmail.com>
2024-04-08 10:30:24 +0200

    LLM + TTS Working
    Integration of LLM calling TTS based on input from user text field
    
    Co-Authored-By: Mads Wagner <79976079+Mads3132@users.noreply.github.com>

---
+++ AI Testing/Assets/LLMUnity/Samples/SimpleInteraction/SimpleInteraction.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using LLMUnity;
+using UnityEngine.UI;
+
+namespace LLMUnitySamples
+{
+    public class SimpleInteraction : MonoBehaviour
+    {
+        public LLM llm;
+        public TextToSpeech tts;
+
+        public InputField playerText;
+        public Text AIText;
+
+        void Start()
+        {
+            playerText.onSubmit.AddListener(onInputFieldSubmit);
+            playerText.Select();
+        }
+
+        void onInputFieldSubmit(string message)
+        {
+            playerText.interactable = false;
+            AIText.text = "...";
+            _ = llm.Chat(message, SetAIText, AIReplyComplete);
+        }
+
+        public void SetAIText(string text)
+        {
+            AIText.text = text;
+            Debug.Log(text);
+            tts.StartGeneration(text);
+        }
+
+        public void AIReplyComplete()
+        {
+            playerText.interactable = true;
+            playerText.Select();
+            playerText.text = "";
+        }
+
+        public void CancelRequests()
+        {
+            llm.CancelRequests();
+            AIReplyComplete();
+        }
+
+        public void ExitGame()
+        {
+            Debug.Log("Exit button clicked");
+            Application.Quit();
+        }
+    }
+}
