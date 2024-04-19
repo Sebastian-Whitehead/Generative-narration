c902b1bd9b0be60a08995fc6685ddc3e0ebc4c5e
Sebastian W. <sebastianmikael2000@gmail.com>
2024-04-19 10:23:48 +0200

    Added listener so that the script dosnt need to be defined in every interractable object

--- Prelim-Testing/Assets/GrabExtension.cs
+++ Prelim-Testing/Assets/GrabExtension.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 [RequireComponent(typeof(Descriptors))]
 public class GrabExtension : MonoBehaviour
 {
     public PipelineManager manager;
+    public XRGrabInteractable xrGrab;
+
     private string ActionDescription = "picked up";
 
     private void Start()
     {
         manager = FindObjectOfType<PipelineManager>();
+        xrGrab = GetComponent<XRGrabInteractable>();
+        xrGrab.selectEntered.AddListener(PassObjToPipeline);
     }
 
-    public void PassObjToPipeline() { 
+    public void PassObjToPipeline(SelectEnterEventArgs args) { 
         manager.trigger(this.gameObject, ActionDescription);
     }
        
